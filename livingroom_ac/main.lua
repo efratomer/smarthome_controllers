69d5e7c1842a9f246051577a2567176dfa7bcc1f
Omer Efrat <efratomer@gmail.com>
2018-10-22 23:49:22 +0300

    livingroom_ac: Remove old mqtt_log option

--- livingroom_ac/main.lua
+++ livingroom_ac/main.lua
@@ -1,6 +1,5 @@
 require "common"
 
-mqttLog = 0
 BUILTIN_LED = 4
 LIGHT_THRESHOLD = 500
 STATUS_CHANGE_DELAY = 500
@@ -10,13 +9,6 @@ STATUS_TOPIC = MQTT_BASE_TOPIC.."/console"
 
 LightStatus = -1
 gpio.mode(BUILTIN_LED, gpio.OUTPUT)
-
-function sendStatus(status)
-  print(status)
-  if mqttLog == 1 then
-    m:publish(STATUS_TOPIC, status, MQTT_QOS_AT_LEAST_ONCE, MQTT_NO_RETAIN, function(client) end)
-  end
-end
 
 function changeState(newState)
   if newState == 1 then
@@ -31,7 +23,6 @@ end
 function sendData(hitCount)
   local ldr = adc.read(0) -- reads value from Analog Input (max 1V)
 
-  sendStatus("ldr=" .. ldr .. ", hitCount=" .. hitCount)
   param_set("ldr", ldr)
   if ldr > LIGHT_THRESHOLD and LightStatus ~= 1 then  -- If sensor >500 detect status LED is on
     if hitCount < TARGET_HIT then
@@ -40,7 +31,6 @@ function sendData(hitCount)
       do return end
     end
     LightStatus = 1
-    sendStatus("ldr=" .. ldr .. ", status=1")
     changeState(1)
   end
   if ldr < LIGHT_THRESHOLD and LightStatus ~= 0 then  -- If sensor >500 detect status LED changed from on to off
@@ -50,7 +40,6 @@ function sendData(hitCount)
       do return end
     end
     LightStatus = 0
-    sendStatus("ldr=" .. ldr .. ", status=0")
     changeState(0)
   end
 end
@@ -60,8 +49,7 @@ function mqtt_on_message(client, topic, data)
 end
 
 function mqtt_on_connect(client)
-  sendStatus("MQTT connected")
-  m:subscribe(MQTT_BASE_TOPIC.."/log", MQTT_QOS_AT_LEAST_ONCE)
+  print("MQTT connected")
   tmr.alarm(0, 1000, 1, function() sendData(0) end )
 end
 
